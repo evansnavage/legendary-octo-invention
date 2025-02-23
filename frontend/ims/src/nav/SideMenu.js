b5f94785b8ee643d8f975951fc878b2badf75acb
cdk6546 <cdk6546@rit.edu>
2025-02-22 22:42:23 -0500

    buttons all done on left nav

--- frontend/ims/src/nav/SideMenu.js
+++ frontend/ims/src/nav/SideMenu.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { stack as Menu } from 'react-burger-menu'
 import './sidemenu.css'
-import { Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class SideMenu extends React.Component {
@@ -13,13 +12,27 @@ class SideMenu extends React.Component {
     return (
     <div>
       <Menu>
-        <div id="home" className="menu-item">
-            <a className="dash-butt"><FontAwesomeIcon icon="fa-solid fa-chart-pie" /></a>
+      <div id="add" className="menu-item">
+            <a id="add" className="butt-large"><FontAwesomeIcon icon="fa-solid fa-plus" /></a>
+            Create
+        </div>
+        <div id="dashboard" className="menu-item">
+            <a id="dashboard" className="butt"><FontAwesomeIcon icon="fa-solid fa-chart-pie" /></a>
             Dashboard
         </div>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        <div id="inventory" className="menu-item">
+            <a id="inventory" className="butt"><FontAwesomeIcon icon="fa-solid fa-warehouse" /></a>
+            Inventory
+        </div>
+        <div id="events" className="menu-item">
+            <a id="events" className="butt"><FontAwesomeIcon icon="fa-solid fa-face-smile" /></a>
+            Events
+        </div>
+
+        <div id="home" className="menu-item">
+            <a id="settings" className="butt"><FontAwesomeIcon icon="fa-solid fa-gear" /></a>
+            Settings
+        </div>
       </Menu>
       </div>
     );
